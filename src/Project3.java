378c77e9f1a955e7f11fc6b948cde115b67b111c
cldrake01 <clrake01@bvsd.org>
2022-08-26 15:00:41 -0600

    .

--- src/Project3.java
+++ src/Project3.java
@@ -4,7 +4,12 @@ public class Project3 {
     public static void main(String[] args) {
         Scanner reader = new Scanner (System.in);
         System.out.print("What distance would you like to convert to nautical miles?: ");
-        double y = reader.nextDouble();
-        System.out.println(y / 1.852);
+        try {
+            double y = reader.nextDouble();
+            System.out.println(y / 1.852);            //  Block of code to try
+        }
+        catch(Exception e) {
+            System.out.println("That's an unsupported input type.");
+        }
     }
 }
