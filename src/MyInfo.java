0cdc4c649442b9b40aa05c88576cc3d8c23f9c4b
collindrake <collinlindendrake@gmail.com>
2022-08-28 15:17:10 -0600

    The program now stops itself should the user's exhaust their three attempts.

--- src/MyInfo.java
+++ src/MyInfo.java
@@ -1,3 +1,4 @@
+//This file is intended to be used in conjunction with Project1.java
 public class MyInfo {
     static String name = "Collin Linden Drake";
 
@@ -6,5 +7,7 @@ public class MyInfo {
     static String address = "2890 Twin Lakes Cir, Lafayette, CO";
 
     static String social_security = "😑";
+
+    static int attempts = 3;
 
 }
