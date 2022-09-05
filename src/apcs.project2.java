bb34fa0dbfb73bb1d5473b2f00fdbf5d5b4b90d6
collindrake <collinlindendrake@gmail.com>
2022-09-05 15:29:22 -0600

    This is the completed version of Project2.

---
+++ src/apcs.project2.java
@@ -0,0 +1,24 @@
+class Project2 {
+    public static void main(String[] args) {
+
+        System.out.println("  ***************  ");
+        System.out.println("   *           *   ");
+        System.out.println("    *  Yield  *    ");
+        System.out.println("     *       *     ");
+        System.out.println("      *     *      ");
+        System.out.println("       *****       ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("         |         ");
+        System.out.println("_________|_________");
+        //System.out.println("");
+    }
+}
