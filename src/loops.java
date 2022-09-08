c4026e5fd6cd62936c0174b14df765894a0de06c
cldrake01 <clrake01@bvsd.org>
2022-09-08 15:10:24 -0600

    //

---
+++ src/loops.java
@@ -0,0 +1,16 @@
+public class loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.print(i + ", ");
+        }
+        for (int i = 7; i < 13; i++) {
+            System.out.print(i + ", ");
+        }
+        for (int i = 12; i < 31; i += 3) {
+            System.out.print(i + ", ");
+        }
+        for (int i = 100; i >= 0 ; i -= 2) {
+            System.out.print(i + ", ");
+        }
+    }
+}
