808a79d66ecda14999781893eec859c61f712037
cldrake01 <clrake01@bvsd.org>
2022-09-15 15:16:36 -0600

    //

--- src/lucky7/Lucky7.java
+++ src/lucky7/Lucky7.java
@@ -6,26 +6,25 @@ public class Lucky7 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("What would you like your balance to be?: ");
+        int[] max = {0, 0};
+
+        System.out.println("Good evening! How much will you be betting with us tonight?: ");
         balance = reader.nextInt();
 
-        System.out.println("Choose your lucky number: ");
+        System.out.println("Excellent, now please choose a lucky number: ");
         int luckyNumber = reader.nextInt();
 
         while (luckyNumber > 12) {
 
-            System.out.println("Choose a number less than twelve, but greater than two: ");
+            System.out.println("Might I suggest choosing a number twelve and two: ");
             luckyNumber = reader.nextInt();
 
         }
 
-        System.out.println("How much will you be betting tonight?: ");
-        balance = reader.nextInt();
-
         for (int roll = 0; balance > 0; roll++) {
 
-            double dieOne = Math.random() * luckyNumber - 1;
-            double dieTwo = Math.random() * luckyNumber - 1;
+            double dieOne = Math.random() * (luckyNumber);
+            double dieTwo = Math.random() * (luckyNumber);
 
             if (Math.round(dieOne) + Math.round(dieTwo) == luckyNumber) {
 
@@ -34,13 +33,18 @@ public class Lucky7 {
                 System.out.println("You won and your balance is now $" + balance + ", would you like to continue playing?: (Y/n) ");
                 String con = reader.next();
 
+                if  (max[0] < balance) {
+                    max[0] = balance;
+                    max[1] = roll;
+                }
+
                 if (con.equals("Y")) {
 
                     continue;
 
                 } else if (con.equals("n")) {
 
-                    System.out.println("You're taking home a glorious sum of " + balance + " dollars, thank you playing with us.");
+                    System.out.println("You're taking home a glorious sum of " + balance + " dollars, but you should've thrown in the towel on roll " + max[1] + " when your balance was $" + max[0] + ". Thank you playing with us.");
                     break;
 
                 } else {
@@ -53,7 +57,7 @@ public class Lucky7 {
             } else {
 
                 balance--;
-                System.out.println(Math.round(dieOne)+ " + " + Math.round(dieTwo) + " is not equal to " + luckyNumber + ".");
+                System.out.println(Math.round(dieOne)+ " + " + Math.round(dieTwo) + " is unfortunately not equal to " + luckyNumber + ".");
 
             }
 
