10d3eee0c6649d488addf431a0b6bd86ea13f832
cldrake01 <clrake01@bvsd.org>
2022-09-21 14:59:46 -0600

    //

--- src/PiProject/Pi.java
+++ src/PiProject/Pi.java
@@ -4,26 +4,36 @@ import java.util.Scanner;
 
 public class Pi {
 
-    static double itteration = 2.0;
+    static double iteration = 2.0;
     static double sum;
+    static int coSum = 0;
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
 
+        Scanner reader = new Scanner(System.in);
         System.out.print("To how many degrees, or digits, of precision would you like to calculate for Pi?: ");
         double precision = reader.nextDouble();
         precision *= precision;
 
+
         for (double i = 1.0; i < precision; i += 2.0) {
 
-            if (itteration % 2 == 0) {
+            if (iteration % 2 == 0) {
                 sum += 4.0 / i;
             } else {
                 sum += -4.0 / i;
             }
 
-            itteration++;
+            iteration++;
             System.out.println(sum);
+
+        }
+
+        for (int i = 0; i <= 11; i++) {
+
+            double[] coordinate = {Math.random() * 2 -1, Math.random() * 2 -1};
+
+            System.out.println("So far " + coSum + "/" + 11 + " of your points were in the circle.");
 
         }
     }
