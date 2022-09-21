2ef7c326a0a953ce50a5d258d3c1a3c30b96bf99
collindrake <collinlindendrake@gmail.com>
2022-09-21 16:26:33 -0600

    //

--- src/PiProject/Pi.java
+++ src/PiProject/Pi.java
@@ -1,27 +1,31 @@
 package PiProject;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pi {
 
     static double iteration = 2.0;
     static double sum;
-    static int coSum = 0;
+    static double totalCoordinatePairsPassed = 0;
 
     public static void main(String[] args) {
-
         Scanner reader = new Scanner(System.in);
+
         System.out.print("To how many degrees, or digits, of precision would you like to calculate for Pi?: ");
         double precision = reader.nextDouble();
         precision *= precision;
 
-
         for (double i = 1.0; i < precision; i += 2.0) {
 
             if (iteration % 2 == 0) {
+
                 sum += 4.0 / i;
+
             } else {
+
                 sum += -4.0 / i;
+
             }
 
             iteration++;
@@ -29,11 +33,24 @@ public class Pi {
 
         }
 
-        for (int i = 0; i <= 11; i++) {
+        System.out.println("How many coordinate pairs would you like to use for his approximation?: ");
+        int totalCoordinatePairs = reader.nextInt();
 
-            double[] coordinate = {Math.random() * 2 -1, Math.random() * 2 -1};
+        for (int i = 0; i <= totalCoordinatePairs; i++) {
 
-            System.out.println("So far " + coSum + "/" + 11 + " of your points were in the circle.");
+            double[] coordinate = {Math.random() * 2 - 1, Math.random() * 2 - 1};
+
+            if (coordinate[0] + coordinate[1] > 1 || coordinate[0] + coordinate[1] < -1) {
+
+                continue;
+
+            } else {
+
+                totalCoordinatePairsPassed++;
+
+            }
+
+            System.out.println("So far " + ((int) totalCoordinatePairsPassed) + "/" + totalCoordinatePairs + " of your points were in the circle, which translates to " + (totalCoordinatePairsPassed / totalCoordinatePairs) * 4 + " in terms of Pi.");
 
         }
     }
