75f0724dcf33e177cf7869cbad0ba28c76680c38
collindrake <collinlindendrake@gmail.com>
2022-09-25 21:46:47 -0600

    //

--- src/PiProject/Pi.java
+++ src/PiProject/Pi.java
@@ -13,6 +13,7 @@ public class Pi {
 
         System.out.print("To how many degrees, or digits, of precision would you like to calculate for Pi?: ");
         double precision = reader.nextDouble();
+        int digits = (int) precision;
         precision *= precision;
 
         for (double i = 1.0; i < precision; i += 2.0) {
@@ -28,11 +29,12 @@ public class Pi {
             }
 
             iteration++;
-            System.out.println(sum);
 
         }
 
-        System.out.println("How many coordinate pairs would you like to use for his approximation?: ");
+        System.out.println("With " + digits + " digits of precision, your approximation of pi was " + sum + ".");
+
+        System.out.println("How many coordinate pairs would you like to use for your coordinate-pair approximation?: ");
         int desiredNumberOfCoordinatePairs = reader.nextInt();
 
         for (int i = 0; i <= desiredNumberOfCoordinatePairs; i++) {
@@ -48,8 +50,40 @@ public class Pi {
                 totalCoordinatePairsPassed++;
 
             }
+        }
 
-            System.out.println("So far " + ((int) totalCoordinatePairsPassed) + "/" + desiredNumberOfCoordinatePairs + " of your points were in the circle, which translates to " + (totalCoordinatePairsPassed / desiredNumberOfCoordinatePairs) * 4.0 + " in terms of Pi.");
+        System.out.println(((int) totalCoordinatePairsPassed) + "/" + desiredNumberOfCoordinatePairs + " of your points were in the circle, which translates to " + (totalCoordinatePairsPassed / desiredNumberOfCoordinatePairs) * 4.0 + " in terms of Pi.");
+
+        // This allows the user to utilize the approximations of pi in calculating the volume of the earth.
+        System.out.println();
+        System.out.println("Would you like to utilize your new values in some calculations?: [Y/n] ");
+
+        if (reader.next().equalsIgnoreCase("y")) {
+
+            System.out.println("Would you prefer to use your first, or your second approximation?: [First/Second] ");
+            String firstOrSecond = reader.next();
+
+            if (firstOrSecond.equalsIgnoreCase("first")) {
+
+                System.out.println("If we substitute pi for your first approximation of pi in order to calculate the volume of the earth in miles cubed, we'd receive " + (4.0 / 3.0) * sum * (3958.8 * 3958.8 * 3958.8) + " in scientific notation. The actual volume of earth in miles cubed however is " + (4.0 / 3.0) * Math.PI * (3958.8 * 3958.8 * 3958.8) + " in scientific notation.");
+
+            } else if (firstOrSecond.equalsIgnoreCase("second")) {
+
+                System.out.println("If we use your second approximation to calculate the volume of earth it would be " + (4.0 / 3.0) * ((totalCoordinatePairsPassed / desiredNumberOfCoordinatePairs) * 4.0) * (3958.8 * 3958.8 * 3958.8) + " miles cubed in scientific notation! As opposed to the actual volume of earth, which is " + (4.0 / 3.0) * Math.PI * (3958.8 * 3958.8 * 3958.8) + " miles cubed in scientific notation.");
+
+            } else {
+
+                System.out.println("That's not a valid input, in that case, I'll just use both...");
+                System.out.println();
+                System.out.println("If we substitute pi for your first approximation of pi in order to calculate the volume of the earth in miles cubed, we'd receive " + (4.0 / 3.0) * sum * (3958.8 * 3958.8 * 3958.8) + " in scientific notation. The actual volume of earth in miles cubed however is " + (4.0 / 3.0) * Math.PI * (3958.8 * 3958.8 * 3958.8) + " in scientific notation.");
+                System.out.println();
+                System.out.println("If we use your second approximation to calculate the volume of earth it would be " + (4.0 / 3.0) * ((totalCoordinatePairsPassed / desiredNumberOfCoordinatePairs) * 4.0) * (3958.8 * 3958.8 * 3958.8) + " miles cubed in scientific notation! As opposed to the actual volume of earth, which is " + (4.0 / 3.0) * Math.PI * (3958.8 * 3958.8 * 3958.8) + " miles cubed in scientific notation.");
+
+
+            }
+        } else {
+
+            System.out.println("That's just fine, I hope you'll make use of this program again.");
 
         }
     }
