63c1f75eece1ab49144ffceb8b3e73fd89bf9bab
Collin Drake <collinlindendrake@gmail.com>
2022-10-05 19:15:36 -0600

    Update Student.java

--- src/student/Student.java
+++ src/student/Student.java
@@ -5,8 +5,8 @@ import java.util.OptionalDouble;
 
 public class Student {
 
-    String[] classes = {"Math", "Science", "English", "Civics", "Foreign Language"};
-    String[] electives = {"AP CS", "Data Structures", "AP Art History", "Digital Art", "Study Hall"};
+    String[] schedule = {"Math", "English", "Science", "Civics", "Foreign Language", " ", " ", " "};
+    final String[][] electives = {{"AP Computer Science", "Data Structures"}, {"Study Hall", "AP Art History"}, {"Weights & Conditioning", "Walking & Life Fitness"}};
     double id = Math.random() * 1_000_000;
     String name;
 
@@ -40,5 +40,14 @@ public class Student {
 
     public OptionalDouble getHighestScore() {
         return Arrays.stream(this.tests).max();
+    }
+
+    public String getSchedule() {
+        for (int i = 5; i < 8; i++) {
+            for (int arrayIncrement = 0; arrayIncrement < i - 4; arrayIncrement++) {
+                this.schedule[i] = this.electives[arrayIncrement] [(int) (Math.random() * 2)];
+            }
+        }
+        return Arrays.toString(this.schedule);
     }
 }
