e91d9357b2c7aaee98694099dad8c8bfd98bc37c
cldrake01 <clrake01@bvsd.org>
2022-10-07 13:04:33 -0600

    //

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,10 +1,10 @@
 package student;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.OptionalDouble;
 
 public class Student {
-
     String name;
     String[] schedule = {"Math", "English", "Science", "Civics", "Foreign Language", " ", " ", " "};
     final String[][] electives = {{"AP Computer Science", "Data Structures"}, {"Study Hall", "AP Art History"}, {"Weights & Conditioning", "Walking & Life Fitness"}};
@@ -43,9 +43,16 @@ public class Student {
     }
 
     public String getSchedule() {
-        for (int i = this.schedule.length - 4; i < this.schedule.length; i++) {
-            for (int arrayIncrement = 0; arrayIncrement < i - 4; arrayIncrement++) {
-                this.schedule[i] = this.electives[arrayIncrement] [(int) (Math.random() * 2)];
+
+        int electivesIncrement = 0;
+
+        for (String i: this.schedule) {
+
+            if (this.schedule[Integer.parseInt(i)].equals(" ")) {
+
+                this.schedule[Integer.parseInt(i)] = this.electives[electivesIncrement] [(int) (Math.random() * 2)];
+                electivesIncrement++;
+
             }
         }
         return Arrays.toString(this.schedule);
