e34f2f2fc746215eeba5c1b4a203c53ab128f9c7
collindrake <collinlindendrake@gmail.com>
2022-10-16 20:51:27 -0600

    //

--- src/fract/FractionRunner.java
+++ src/fract/FractionRunner.java
@@ -4,7 +4,7 @@ public class FractionRunner {
     public static void main(String[] args) {
         Fraction fractal = new Fraction();
 
-        fractal.setFraction("2/5");
+        //fractal.setFraction("2/5");
         fractal.multiplyByFraction(3, 4);
 
         System.out.println("Numerator: " + fractal.getNumerator());
