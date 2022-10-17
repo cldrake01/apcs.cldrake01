a039f214acacd7a997dace37dd5552fe4d490f85
collindrake <collinlindendrake@gmail.com>
2022-10-16 20:33:32 -0600

    //

---
+++ src/fract/FractionRunner.java
@@ -0,0 +1,19 @@
+package fract;
+
+public class FractionRunner {
+    public static void main(String[] args) {
+        Fraction fractal = new Fraction();
+
+        fractal.setFraction("2/5");
+        fractal.multiplyByFraction(3, 4);
+
+        System.out.println("Numerator: " + fractal.getNumerator());
+        System.out.println("Denominator: " + fractal.getDenominator());
+
+        fractal.divideByFraction(3, 4);
+
+        System.out.println("Fraction: " + fractal.getFractionAsString());
+        System.out.println("Mixed Form: " + fractal.mixedFraction);
+
+    }
+}
