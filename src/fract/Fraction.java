a88aa6f65382c90153a012b6e0bb7a6aa054067f
Collin Drake <collinlindendrake@gmail.com>
2022-10-17 22:42:16 -0600

    Update Fraction.java

--- src/fract/Fraction.java
+++ src/fract/Fraction.java
@@ -3,31 +3,50 @@ package fract;
 public class Fraction {
     public double numerator = 1.0;
     public double denominator = (Math.random() * 10) * Math.random() + 2;
-
-    public String mixedFraction = "";
+    public double mixedFraction = 0;
 
     //Setters
 
-    public void setNumerator(int numerator) {
+    public void setNumerator (int numerator) {
         this.numerator = numerator;
     }
 
-    public void setDenominator(int denominator) {
+    public void setDenominator (int denominator) {
         this.denominator = denominator;
     }
 
-    public void setFraction(String input) {
-        this.numerator = Integer.parseInt(input.substring(0, input.indexOf("/")));
-        this.denominator = Integer.parseInt(input.substring(input.indexOf("/") + 1));
+    /*
+    Here I'm using a method overloading technique to compensate for the non-optional mixedFraction parameter.
+     */
+
+    public void setFraction (int numerator,int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public void setFraction (int numerator,int denominator, int mixedFraction) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        this.mixedFraction = mixedFraction;
     }
 
     public void multiplyByInt (int multiplier) {
         this.numerator *= multiplier;
     }
 
-    public void multiplyByFraction (double numer, double denom) {
-        this.numerator *= numer;
-        this.denominator *= denom;
+    public void multiplyByFraction (Fraction frac) {
+        this.numerator *= frac.numerator;
+        this.denominator *= frac.denominator;
+    }
+
+    public void multiplyByFraction (double numerator, double denomenator) {
+        this.numerator *= numerator;
+        this.denominator *= denomenator;
+    }
+
+    public void divideByFraction (Fraction frac) {
+        this.numerator *= frac.denominator;
+        this.denominator *= frac.numerator;
     }
 
     public void divideByFraction (double numer, double denom) {
@@ -37,15 +56,31 @@ public class Fraction {
 
     //Getters
 
-    public int getNumerator() {
+    public String asString () {
+        if (mixedFraction == 0) {
+            return this.getNumerator() + "/" + this.getDenominator();
+        } else {
+            return this.getMixedFraction() + " " + this.getNumerator() + "/" + this.getDenominator();
+        }
+    }
+
+    public int getNumerator () {
         return (int) numerator;
     }
 
-    public int getDenominator() {
+    public int getDenominator () {
         return (int) denominator;
     }
 
-    public String getFractionAsString () {
-        return this.mixedFraction + this.getNumerator() + "/" + this.getDenominator();
+    public int getMixedFraction() {
+        return (int) mixedFraction;
+    }
+
+    public boolean compare (Fraction fract) {
+        return this.numerator == fract.numerator && this.denominator == fract.denominator;
+    }
+
+    public boolean isMixedFraction () {
+        return this.mixedFraction != 0;
     }
 }
