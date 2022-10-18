769482cd20e8a4ba47d0b728c4e31b5c86c72691
Collin Drake <collinlindendrake@gmail.com>
2022-10-17 22:41:35 -0600

    Update FractionRunner.java

--- src/fract/FractionRunner.java
+++ src/fract/FractionRunner.java
@@ -1,19 +1,38 @@
 package fract;
 
+import java.util.Scanner;
+
 public class FractionRunner {
     public static void main(String[] args) {
-        Fraction fractal = new Fraction();
+        Scanner reader = new Scanner(System.in);
 
-        //fractal.setFraction("2/5");
-        fractal.multiplyByFraction(3, 4);
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction();
 
-        System.out.println("Numerator: " + fractal.getNumerator());
-        System.out.println("Denominator: " + fractal.getDenominator());
+        System.out.println("Please enter a fraction: " + reader.next());
 
-        fractal.divideByFraction(3, 4);
+        System.out.println("f1: " + f1.asString() + "f2: " + f2.asString());
 
-        System.out.println("Fraction: " + fractal.getFractionAsString());
-        System.out.println("Mixed Form: " + fractal.mixedFraction);
+        f1.setNumerator(2);
+        f1.setDenominator(4);
+        f1.setFraction(2, 4);
+
+        System.out.println("f1: " + f1.asString() + " f2: " + f2.asString());
+
+        f1.multiplyByInt(3);
+
+        System.out.println("f1: " + f1.asString() + " f2: " + f2.asString());
+
+        f1.multiplyByFraction(f2);
+
+        System.out.println("f1: " + f1.asString() + " f2: " + f2.asString());
+
+        f1.divideByFraction(f2);
+
+        System.out.println("f1: " + f1.asString() + " f2: " + f2.asString());
+
+        System.out.println("f1, is it a mixed fraction?: " + f1.isMixedFraction() + " f2, is it a mixed fraction?: " + f2.isMixedFraction());
+        System.out.println("Does f1 equal f2?: " + f1.compare(f2));
 
     }
 }
