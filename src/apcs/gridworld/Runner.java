47b39b1cddf8f9af24ceae67a0d2f67d3a0e7290
Collin Drake <collinlindendrake@gmail.com>
2022-10-27 13:39:37 -0600

    //

--- src/apcs/gridworld/Runner.java
+++ src/apcs/gridworld/Runner.java
@@ -1,7 +1,17 @@
 package apcs.gridworld;
 
-public class Runner {
-    public static void main(String[] args) {
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.UnboundedGrid;
 
+
+public class Runner {
+    public static <Actor> void main(String[] args) {
+        UnboundedGrid grid = new UnboundedGrid<Actor>();
+        ActorWorld world = new ActorWorld(grid);
+        SpiralBug sp = new SpiralBug(3);
+        world.add(new Location(3,5),sp);
+        world.show();
     }
 }
---
+++ src/apcs/gridworld/SpiralBug.java
@@ -0,0 +1,28 @@
+package apcs.gridworld;
+
+public class SpiralBug extends Bug
+{
+    private int sideLength;
+    private int steps;
+    public SpiralBug(int n)
+    {
+        sideLength = n;
+        steps = 0;
+    }
+    public void act()
+    {
+        if (steps < sideLength && canMove())
+        {
+            move();
+            steps++;
+        }
+        else
+        {
+            turn();
+            turn();
+            steps = 0;
+            //Each time a SpiralBug turns, increase the sideLength by one
+            sideLength++;
+        }
+    }
+}
