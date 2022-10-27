719c40090f2bb831699c9708c3b5579e1835f3ff
cldrake01 <clrake01@bvsd.org>
2022-10-27 14:18:26 -0600

    //

--- src/apcs/gridworld/SpiralBug.java
+++ src/apcs/gridworld/SpiralBug.java
@@ -6,20 +6,25 @@ public class SpiralBug extends Bug {
     private int steps;
 
     public SpiralBug(int n) {
+
         sideLength = n;
         steps = 0;
+
     }
 
     public void act() {
         if (steps < sideLength && canMove()) {
+
             move();
             steps++;
+
         } else {
+
             turn();
             turn();
             steps = 0;
-            //Each time a SpiralBug turns, increase the sideLength by one
             sideLength++;
+
         }
     }
 }
