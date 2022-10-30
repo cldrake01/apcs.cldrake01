b32beb9987f46d4db5eb12a53cf25644d188211b
Collin Drake <collinlindendrake@gmail.com>
2022-10-29 20:29:28 -0600

    //

--- src/apcs/gridworld/Runner.java
+++ src/apcs/gridworld/Runner.java
@@ -2,15 +2,20 @@ package apcs.gridworld;
 
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 import info.gridworld.grid.UnboundedGrid;
+
+import java.util.ArrayList;
 
 
 public class Runner {
     public static <Actor> void main(String[] args) {
-        UnboundedGrid grid = new UnboundedGrid<Actor>();
-        ActorWorld world = new ActorWorld(grid);
-        SpiralBug sp = new SpiralBug(3);
+        ActorWorld griddy = new ActorWorld();
+        ActorWorld world = new ActorWorld(griddy.getGrid());
+        SpiralBug sp = new SpiralBug(1);
+        Bug bu = new Bug();
+        world.add(new Location(2,4),bu);
         world.add(new Location(3,5),sp);
         world.show();
     }
