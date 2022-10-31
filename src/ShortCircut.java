c986d4758d289af7d533982dcb13e66f3a49220d
cldrake01 <clrake01@bvsd.org>
2022-10-31 15:03:11 -0600

    //

---
+++ src/ShortCircut.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ShortCircut {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Please enter an integer: ");
+            int input = in.nextInt();
+
+            if (input != 0 && ((input / 100) > 5)) {
+                System.out.println("success");
+            } else {
+                System.out.println("failure");
+            }
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("That input doesn't divide properly, try again.");
+
+            main(null);
+
+        } catch (InputMismatchException e) {
+
+            String error = in.nextLine();
+            System.out.println(error + " is not an integer, please enter a number instead.");
+
+            main(null);
+
+        }
+    }
+}
