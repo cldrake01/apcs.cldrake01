d1b519de8ecfc1b468bb3bb5cfa6c6a0d750e8ec
Collin Drake <collinlindendrake@gmail.com>
2022-11-08 12:04:25 -0700

    //

---
+++ src/frq/sfdkl.java
@@ -0,0 +1,45 @@
+package frq;
+
+public class sfdkl {
+    public static void main(String[] args) {
+        String str1 = new String("Advanced Placement");
+
+        String str2 = new String("Advanced Placement");
+
+        /*
+        if (str1.equals(str2) && str1 == str2)
+
+        {
+
+            System.out.println("A");
+
+        }
+
+        else if (str1.equals(str2) && str1 != str2)
+
+        {
+
+            System.out.println("B");
+
+        }
+
+        else if (!str1.equals(str2) && str1 == str2)
+
+        {
+
+            System.out.println("C");
+
+        }
+
+        else if (!str1.equals(str2) && str1 != str2)
+
+        {
+
+            System.out.println("D");
+
+        }
+        */
+
+        System.out.println(str1 == str2);
+    }
+}
