3d887f28757f7dfa9d2a856cfd617b8108b140be
cldrake01 <clrake01@bvsd.org>
2022-12-08 15:56:22 -0700

    //

--- src/frq/SoundRunner.java
+++ src/frq/SoundRunner.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 
 public class SoundRunner {
     public static void main(String[] args) {
+
         Sound sound = new Sound();
+        System.out.println("Before trimSilenceFromBeginning(): " + Arrays.toString(sound.samples));
         sound.trimSilenceFromBeginning();
-        System.out.println(Arrays.toString(sound.samples));
-        System.out.println(sound.limitAmplitude(32));
-        System.out.println(Arrays.toString(sound.samples));
+        System.out.println("After trimSilenceFromBeginning(): " + Arrays.toString(sound.samples));
+        System.out.println("Changes made with amplitudeLimit(): " + sound.limitAmplitude(32));
+        System.out.println("Final Array: " + Arrays.toString(sound.samples));
 
     }
 }
