ddeeb19b56499fdaff3545ddd6e8757127d4a556
Collin Drake <collinlindendrake@gmail.com>
2022-12-09 13:31:26 -0700

    //

--- src/frq/Sound.java
+++ src/frq/Sound.java
@@ -7,17 +7,18 @@ public class Sound {
         public int limitAmplitude(int limit) {
 
                 int changes = 0;
-
-                for(int amplitude = 0; amplitude < this.samples.length; amplitude++) {
-
-                        if (this.samples[amplitude] > limit || this.samples[amplitude] < -limit) {
+                // This code segment is fairly self-explanatory, it counts the changes made to samples with the variable
+                // changes, adding to changes when a variable exceeds the permitted amplitude, e.g.
+                // samples[indexOfVarToBeChecked] cannot be greater than, or less than, limit or -limit.
+                for(int indexOfVarToBeChecked = 0; indexOfVarToBeChecked < this.samples.length; indexOfVarToBeChecked++) {
+                        if (this.samples[indexOfVarToBeChecked] > limit || this.samples[indexOfVarToBeChecked] < -limit) {
 
                                 changes++;
 
-                                if (this.samples[amplitude] > limit) {
-                                        this.samples[amplitude] = limit;
-                                } else if (this.samples[amplitude] < -limit) {
-                                        this.samples[amplitude] = -limit;
+                                if (this.samples[indexOfVarToBeChecked] > limit) {
+                                        this.samples[indexOfVarToBeChecked] = limit;
+                                } else if (this.samples[indexOfVarToBeChecked] < -limit) {
+                                        this.samples[indexOfVarToBeChecked] = -limit;
                                 }
                         }
                 }
@@ -26,18 +27,23 @@ public class Sound {
         }
 
         public void trimSilenceFromBeginning() {
-                int index = 0;
+
+                // This code segment sets the variable zeroIndex equal to the index of the first non-zero integer.
+                int zeroIndex = 0;
                 for (int i = 0; i < this.samples.length; i++) {
                         if (this.samples[i] != 0) {
-                                index = i;
+                                zeroIndex = i;
                                 break;
                         }
                 }
 
-                int[] newArray = new int[this.samples.length - (index + 1)];
+                int[] newArray = new int[this.samples.length - (zeroIndex + 1)];
 
+                // This code segment sets the entirety of newArray to the values of sample beginning at zeroIndex, e.g.
+                // samples[zeroIndex], newArray is then looped through beginning at 0, e.g. newArray[newArrayIndex] or
+                // more simply newArray[0].
                 int newArrayIndex = 0;
-                for (int i = index; i < this.samples.length - index + 1; i++) {
+                for (int i = zeroIndex; i < this.samples.length - zeroIndex + 1; i++) {
                         newArray[newArrayIndex] = this.samples[i];
                         newArrayIndex++;
                 }
