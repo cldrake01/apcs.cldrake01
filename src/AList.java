58c9c34f855d75d27913aad525f0dedda0f043c6
cldrake01 <clrake01@bvsd.org>
2023-01-09 14:47:26 -0700

    //

--- src/AList.java
+++ src/AList.java
@@ -9,17 +9,34 @@ public class AList {
         this.list.addAll(List.of(list));
     }
 
-    public static void populateList (List list, int times) {
-        for( int i = 0; i < times; i++) {
-            list.add((double) (Math.round(Math.random() * list.size())));
-        };
+    public static void populateList(List list, int times) {
+        for (int i = 0; i < times; i++) {
+            list.add((double) (Math.round(Math.random() * list.size() + 1)));
+        }
+        ;
     }
 
     public static void main(String[] args) {
-        AList l = new AList();
+        AList l = new AList(80., 29., 33., 49., 26.);
         l.populateList(l.list, 20);
         System.out.println(l.list);
         System.out.println(l.list.size());
 
+        for (int i = 1; i < l.list.size(); i += 2) {
+            l.list.add(i, 0.0);
+        }
+
+        System.out.println(l.list);
+        System.out.println(l.list.size());
+
+        for (int i = 0; i < l.list.size(); i++) {
+            if (l.list.get(i) == 0.0) {
+                l.list.remove(i);
+                i--;
+            }
+        }
+
+        System.out.println(l.list);
+        System.out.println(l.list.size());
     }
 }
