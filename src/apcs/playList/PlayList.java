66da26630aa94db4213a3dc28092a527ef636a01
cldrake01 <clrake01@bvsd.org>
2023-01-11 15:08:00 -0700

    //

--- src/apcs/playList/PlayList.java
+++ src/apcs/playList/PlayList.java
@@ -6,9 +6,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayList {
+    String name;
     ArrayList<Song> songs = new ArrayList<Song>();
 
+    public PlayList() {
+        this.name = "New Playlist";
+    }
+
+    public PlayList(Song @NotNull ... songs) {
+        this.songs.addAll(List.of(songs));
+    }
+
     public PlayList(String name, Song @NotNull ... songs) {
-        songs.addAll(songs);
+        this.name = name;
+        this.songs.addAll(List.of(songs));
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public void add(Song... songs) {
+        this.songs.addAll(List.of(songs));
+    }
+
+    @Override
+    public String toString() {
+        return "PlayList{" +
+                "songs=" + songs +
+                '}';
     }
 }
---
+++ src/apcs/playList/PlayListTester.java
@@ -0,0 +1,11 @@
+package apcs.playList;
+
+public class PlayListTester {
+    public static void main(String[] args) {
+        PlayList playList = new PlayList();
+
+        System.out.println(playList);
+
+
+    }
+}
