952e7c02eb297dfc461862c46061222f689ce1f9
Collin Drake <collinlindendrake@gmail.com>
2023-04-07 09:40:54 -0600

    This cardioid code is God awful.

--- src/apcs/turtles/shapes/Cardioid.java
+++ src/apcs/turtles/shapes/Cardioid.java
@@ -14,7 +14,7 @@ public class Cardioid extends Circle implements Shape {
         for (int i = -2; i < 90; i++) {
             for (int j = -2; j < 90; j++) {
                 r = Math.PI / 45 + Math.PI / 45 * i * (1 - Math.sin(Math.PI / 45 * j)) * 18;
-                x = r * Math.cos(Math.PI / 45 * j) * Math.sin(Math.PI / 45 * i) + this.radius/ 2;
+                x = r * Math.cos(Math.PI / 45 * j) * Math.sin(Math.PI / 45 * i) + this.radius / 2;
                 y = -r * Math.sin(Math.PI / 45 * j) + this.radius / 3;
             }
         }
