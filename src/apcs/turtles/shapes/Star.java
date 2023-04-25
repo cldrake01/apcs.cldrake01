557707b1fba7f988ddfa95ba2027ea52d04a8e98
Collin Drake <collinlindendrake@gmail.com>
2023-04-25 12:35:19 -0600

    Code: Written Documentation: Added Relationships Sacrificed: 4 Future: Exception in thread "main" java.lang.NullPointerException: Cannot invoke "Life.prosper()" because "future" is null         	at apcs.turtles.TurtleRunner.main(TurtleRunner.java:23)

---
+++ src/apcs/turtles/shapes/Star.java
@@ -0,0 +1,55 @@
+package apcs.turtles.shapes;
+
+import TurtleGraphics.Pen;
+import TurtleGraphics.SketchPadWindow;
+import apcs.turtles.SlowPen;
+
+public class Star implements Shape {
+
+    private double xPos, yPos, points, sideLength;
+
+    private final Pen pen = new SlowPen(new SketchPadWindow(1920, 1080));
+
+    public Star(double sides, double sideLength) {
+        this.points = sides;
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getXPos() {
+        return xPos;
+    }
+
+    @Override
+    public double getYPos() {
+        return yPos;
+    }
+
+    @Override
+    public void move(double xPos, double yPos) {
+
+    }
+
+    @Override
+    public Shape stretchBy(double factor) {
+        return null;
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public void draw() {
+        // Set up the pen
+        getInfo(pen);
+        reset(pen);
+
+        // Draw the polygon
+        for (int i = 0; i < this.points; i++) {
+            pen.move(this.sideLength);
+            pen.turn(180 - (360 / (this.points * 2)));
+        }
+    }
+}
